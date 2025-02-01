442c89109ef13f7c8478a1274e3b4f84d9492876
Michael Amponsah <nanrmichael@gmail.com>
2025-02-01 12:04:29 +0000

    update tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -28,34 +28,7 @@ const config: Config = {
       abel: ["var(--font-abel)"],
     },
     extend: {
-    colors: {
-      primary: "#1D57C2",
-      secondary: "#4C9FE4",
-      text: "#413F3F",
-      "dark-text": "#F5F5F5",
-      "subtle-text": "#939393",
-      "light-shade": "#1D57C21A",
-      dark: "#070707",
-      gray: "#0B0F12",
-      white: "#FFFFFF",
-      transparent: "transparent",
-    },
-    fontFamily: {
-      sans: ["var(--font-roboto-flex)"],
-      "space-grotesk": ["var(--font-space-grotesk)"],
-      "roboto-flex": ["var(--font-roboto-flex)"],
-      afacad: ["var(--font-afacad)"],
-      abel: ["var(--font-abel)"],
-    },
-    extend: {
       screens: {
-        xs: "320px",
-        sm: "640px",
-        md: "768px",
-        lg: "1280px",
-        xl: "1440px",
-        "2xl": "1920px",
-        "3xl": "3840px",
         xs: "320px",
         sm: "640px",
         md: "768px",
@@ -71,20 +44,12 @@ const config: Config = {
           },
         },
         "accordion-down": {
-          from: {
-            height: "0",
-          },
-          to: {
-            height: "var(--radix-accordion-content-height)",
-          },
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
-          from: {
-            height: "var(--radix-accordion-content-height)",
-          },
-          to: {
-            height: "0",
-          },
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
       },
       animation: {
