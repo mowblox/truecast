acf9822458d36ce06f6b900289521dd9d8f996d8
Michael Amponsah <nanrmichael@gmail.com>
2025-02-04 22:22:26 +0000

    feat: vote verify link page

--- frontend/src/app/dashboard/create-election/_components/inputs/InputWrapper.tsx
+++ frontend/src/app/dashboard/create-election/_components/inputs/InputWrapper.tsx
@@ -1,17 +1,25 @@
+import { cn } from "@/lib/utils";
 import React, { ReactNode } from "react";
 
 const InputWrapper = ({
   name,
   label,
   children,
+  labelClass,
+  className,
 }: {
   name: string;
   label: string;
   children: ReactNode;
+  labelClass?: string;
+  className?: string;
 }) => {
   return (
-    <div className="flex flex-col gap-3">
-      <label htmlFor={name} className="text-lg dark:text-white/87">
+    <div className={cn("flex flex-col gap-3", className)}>
+      <label
+        htmlFor={name}
+        className={cn("text-lg dark:text-white/87", labelClass)}
+      >
         {label}
       </label>
       {children}
--- frontend/src/app/dashboard/vote/page.tsx
+++ frontend/src/app/dashboard/vote/page.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import InputWrapper from "../create-election/_components/inputs/InputWrapper";
+import TextInput from "../create-election/_components/inputs/TextInput";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <form className="mx-32 max-md:mx-4 max-w-2xl flex flex-col gap-24">
+      <InputWrapper
+        labelClass="text-white/60 text-3xl mb-1"
+        label="Check eligibility"
+        name="link"
+      >
+        <input
+          required
+          className="p-6 bg-[#1D57C21A] rounded-md text-secondary placeholder:italic placeholder:text-secondary outline-none"
+          placeholder="Type or paste unique voting link"
+        />
+      </InputWrapper>
+
+      <div className="flex justify-end">
+        <button
+          type="submit"
+          className="w-fit px-8 py-2.5 rounded-full bg-primary text-white text-lg"
+        >
+          Verify
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default page;
