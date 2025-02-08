e3ff961d88acf539a6ffc33de1a1e4c225d1bcd3
Michael Amponsah <nanrmichael@gmail.com>
2025-02-08 14:14:53 +0000

    fix: restore form on vote page

--- frontend/src/app/dashboard/vote/page.tsx
+++ frontend/src/app/dashboard/vote/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-import InputWrapper from "../create-election/_components/inputs/InputWrapper";
+import CheckEligibilityForm from "./_components/CheckEligibilityForm";
 
 const page = () => {
-  return <main className="mx-32 max-md:mx-4 max-w-2xl"></main>;
+  return (
+    <main className="mx-32 max-md:mx-4 max-w-2xl">
+      <CheckEligibilityForm />
+    </main>
+  );
 };
 
 export default page;
