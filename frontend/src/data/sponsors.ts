e0a5b90c4ed2a39398ee0c72346978bcce1504c3
Michael Amponsah <nanrmichael@gmail.com>
2025-03-01 00:13:48 +0000

    update sponsor logos

---
+++ frontend/src/data/sponsors.ts
@@ -0,0 +1,12 @@
+export const sponsors = [
+  {
+    name: "Scroll",
+    image: "/images/logos/sponsors/scroll_light.svg",
+    imageDark: "/images/logos/sponsors/scroll_dark.svg",
+  },
+  {
+    name: "Creya",
+    image: "/images/logos/sponsors/creya_light.svg",
+    imageDark: "/images/logos/sponsors/creya_dark.svg",
+  },
+];
