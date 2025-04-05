7345af90d7004c04503bdf66e74f976686e51b36
mikehammond <54583027+mikehammond@users.noreply.github.com>
2025-04-05 22:00:48 +0000

    adding other networks

--- frontend/src/components/WalletProvider.tsx
+++ frontend/src/components/WalletProvider.tsx
@@ -5,18 +5,20 @@ import { darkTheme, getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rai
 import { WagmiProvider, http } from 'wagmi';
 import { rainbowWallet, metaMaskWallet, walletConnectWallet } from '@rainbow-me/rainbowkit/wallets';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { sepolia, scrollSepolia, mainnet } from 'wagmi/chains';
+import { sepolia, scrollSepolia, mainnet, lineaSepolia, hoodi } from 'wagmi/chains';
 
 const queryClient = new QueryClient();
 
 const config = getDefaultConfig({
   appName: 'TrueCast',
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
-  chains: [mainnet, sepolia, scrollSepolia],
+  chains: [mainnet, sepolia, hoodi, scrollSepolia, lineaSepolia],
   transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
-    [scrollSepolia.id]: http(),
+    [mainnet.id]: http('https://ethereum-rpc.publicnode.com'),
+    [sepolia.id]: http('https://ethereum-sepolia-rpc.publicnode.com'),
+    [hoodi.id]: http('https://ethereum-hoodi-rpc.publicnode.com'),
+    [scrollSepolia.id]: http('https://sepolia-rpc.scroll.io'),
+    [lineaSepolia.id]: http('https://rpc.sepolia.linea.build'),
   },
   wallets: [{
     groupName: 'Recommended',
