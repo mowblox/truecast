801f1b3279d89befdfc1482f25bd10bc3c6edcc7
Michael Hammond <54583027+mikehammond@users.noreply.github.com>
2025-10-13 11:27:38 +0000

    Using chainId in voting link to switch chain

--- frontend/src/app/dashboard/vote/[id]/page.tsx
+++ frontend/src/app/dashboard/vote/[id]/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Candidates } from "./_components/Candidates";
-import Header from "./_components/Header";
+import dynamic from "next/dynamic";
+
+const NoSSRHeader = dynamic(() => import('./_components/Header'), { ssr: false })
 
 const page = () => {
   return (
     <main className="mx-[117px] max-md:mx-4 flex flex-col gap-[62px]">
-      <Header />
+      <NoSSRHeader />
       <div className="flex flex-col gap-8">
         <Candidates />
       </div>
