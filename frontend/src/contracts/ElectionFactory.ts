d5c7747b451884bba55d35849e8a14947e69edd5
Michael Hammond <54583027+mikehammond@users.noreply.github.com>
2025-10-27 07:57:09 +0000

    Deploying to other testnets

--- frontend/src/contracts/ElectionFactory.ts
+++ frontend/src/contracts/ElectionFactory.ts
@@ -7,13 +7,13 @@ export const getFactoryAddress = (chainId: number) => {
     case 11155111:
       return '0x5c4b9b14f30a2d320b81044Ab1a0e407c9b6833C'; // sepolia
     case 17000:
-      return '0x9fca09E8579C43753f37727Ba87d5e4eAbE2EDaD'; // holesky
+      return '0x73d7175176e8297db4Ac92eAD6521Ff440f25F13'; // holesky
     case 43113:
       return '0xe70723B89e1c9ccCA17b7a9D2149BC6C3DEB01CB'; // avalanche_fuji
     case 534351:
-      return '0x255e1031D59a200FEDc7DE3Bb0205493685297b8'; // scroll_sepolia
+      return '0x4166Dcb4Ca44A67Ab6299fA0834e0e44Bb6203C8'; // scroll_sepolia
     case 59141:
-      return '0x6043DFca8ee0CDD60028e7262f08c1b59f256231'; // linea_sepolia
+      return '0x4E6bFfe6FCc0860c81868f8C739D1D69cE5Ee6BC'; // linea_sepolia
     case 31337:
       return '0xF7E2Be9007fEaAcEB821D98011975A78034cCDC6'; // hardhat
     default:
